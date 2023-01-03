52d19b1beeed1d048232f28346ae902054ec56fd
SilentJMA <kilwanitro@gmail.com>
2023-01-03 11:40:19 +0100

    clean

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,53 +6,53 @@
 /*   By: mjabane <mjabane@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 19:25:01 by mjabane           #+#    #+#             */
-/*   Updated: 2022/05/07 17:57:18 by mjabane          ###   ########.fr       */
+/*   Updated: 2023/01/03 11:26:25 by mjabane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_value(const char *str, va_list ap, int i, int j)
+int	ft_putvalue(const char *str, va_list ap, int i, int t)
 {
 	unsigned long long	p;
 
 	if (str[i] == 'p')
 		p = va_arg(ap, unsigned long long);
-	if (str[i] == 'd' || str[i] = 'i')
-		j += ft_putnbr(va_arg(ap, int));
+	if (str[i] == 'd' || str[i] == 'i')
+		t += ft_putnbr(va_arg(ap, int));
 	else if (str[i] == 's')
-		j += ft_putstr(va_arg(ap, char *));
+		t += ft_putstr(va_arg(ap, char *));
 	else if (str[i] == 'c')
-		j += ft_putchar(va_arg(ap, int));
+		t += ft_putchar(va_arg(ap, int));
 	else if (str[i] == 'p')
-		j += ft_putmem(&p, "0123456789abcdef");
+		t += ft_putmem(&p, "0123456789abcdef");
 	else if (str[i] == 'u')
-		j += ft_putnbr_u(va_arg(ap, unsigned int));
+		t += ft_putnbr_u(va_arg(ap, unsigned int));
 	else if (str[i] == 'x')
-		j += ft_putnbr_base(va_arg(ap, int), "0123456789abcdef");
+		t += ft_putnbr_base(va_arg(ap, int), "0123456789abcdef");
 	else if (str[i] == 'X')
-		j += ft_putnbr_base(va_arg(ap, int), "0123456789ABCDEF");
+		t += ft_putnbr_base(va_arg(ap, int), "0123456789ABCDEF");
 	else if (str[i] == '%')
-		j += ft_putchar('%');
-	return (j);
+		t += ft_putchar('%');
+	return (t);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	ap;
 	int		i;
-	int		j;
+	int		t;
 
-	j = 0;
+	t = 0;
 	i = -1;
 	va_start(ap, str);
 	while (str[++i])
 	{
-		if (str[++i])
-			j += ft_value(str, ap, ++i, 0);
+		if (str[i] == '%')
+			t += ft_putvalue(str, ap, ++i, 0);
 		else
-			j += ft_putchar(str[i]);
+			t += ft_putchar(str[i]);
 	}
 	va_end(ap);
-	return (j);
+	return (t);
 }
